75a649af14b3fd95637981ea72d55c73d74cac34
debaditya.mohankudo <debaditya.mohankudo@digicert.com>
2021-05-14 12:37:10 +0530

    changes

--- src/RelaxNGCompactSchemaValidator.java
+++ src/RelaxNGCompactSchemaValidator.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.function.Function;
+
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -23,43 +23,54 @@ import org.xml.sax.SAXException;
 
 
 class RelaxNGCompactSchemaValidator { //JingTask.Java
-
+    private static File logFile = new File("/tmp/log.txt");
    
 
 
     public static void main(String[] args) throws IOException {
-        List<String> result = validateRnc();
-        System.out.println(formatAllExceptions(result));
+        cleanupLogFile();
+        validateRnc();
+        formatAllExceptions().forEach(System.out::println);
     }
 
-    private static List<List<String>> formatAllExceptions(List<String> result) {
+    private static void cleanupLogFile() throws IOException {
+        if (Files.exists(logFile.toPath())) {
+            Files.delete(logFile.toPath());
+        }
+    }
+
+    private static List<List<String>> formatAllExceptions() throws IOException {
         List<List<String>> allError = new ArrayList<List<String>>(); 
+        
+        List<String> result = Files.readAllLines(logFile.toPath());
+        
+
         System.out.println("No of errors: " + result.size());
+
         Iterator<String> errorIterator = result.iterator();
+
         while (errorIterator.hasNext()) {
             String errorText = errorIterator.next();
             List<String> error = Arrays.asList(errorText.split(";"));
-            error.set(1, "Message: " + error.get(1));
+
             allError.add(error);
         }
+
         return allError;
     }
 
-    public static List<String> validateRnc() throws IOException
+    public static void validateRnc() throws IOException
     {
           // https://stackoverflow.com/questions/4983057/using-jing-with-google-app-engine-cant-load-schemafactory-given-relax-ng-schem
-          File logFile = new File("./log.txt");
           
           File schemaLocation = new File("/Users/debaditya.mohankudo/Documents/SPIKE-validate-BIMI-LOGO/relaxng/relaxng/svg_1-2_ps.rnc");
           File svgLocation = new File("/Users/debaditya.mohankudo/Documents/SPIKE-validate-BIMI-LOGO/relaxng/relaxng/xss-mouseover.svg");
           System.setProperty(SchemaFactory.class.getName() + ":" + XMLConstants.RELAXNG_NS_URI, "com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory");
           SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.RELAXNG_NS_URI);
-          FileWriter sw = new FileWriter(logFile);
-          ErrorHandlerImpl eh = new ErrorHandlerImpl(sw);
+          ErrorHandlerImpl eh = new ErrorHandlerImpl(new FileWriter(logFile));
           
         try 
         {
-            
             Schema schema = factory.newSchema(schemaLocation);
             Validator validator = schema.newValidator();
             validator.setErrorHandler(eh);
@@ -72,10 +83,6 @@ class RelaxNGCompactSchemaValidator { //JingTask.Java
             // TODO Auto-generated catch block
             eh.printException(e);
         }
-
-        List<String> result = Files.readAllLines(logFile.toPath());
-        
-        return result;
     }
 
 
