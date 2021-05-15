bd959244c9cca98629d02c216c78986ce58879e4
debaditya.mohankudo <debaditya.mohankudo@digicert.com>
2021-05-15 11:16:08 +0530

    refactor

--- src/RelaxNGCompactSchemaValidator.java
+++ src/RelaxNGCompactSchemaValidator.java
@@ -20,22 +20,29 @@ import org.xml.sax.SAXException;
 
 class RelaxNGCompactSchemaValidator { 
     
-    public static class ceh extends ErrorHandlerImpl {
+    private  class ceh extends ErrorHandlerImpl {
 
-        public static final List<List<String>> allErrors = new ArrayList<List<String>>();
+        private List<List<String>> allErrors = new ArrayList<List<String>>();
         public void print(String message) {
             if (message.length() != 0) {
                 List<String> error = Arrays.asList(message.split(";"));
                 allErrors.add(error);
             }
         }
+
+        public List<List<String>> getAllErrors() {
+            return allErrors;
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        validateRnc();
+        RelaxNGCompactSchemaValidator obj = new RelaxNGCompactSchemaValidator();
+        List<List<String>> allErros = obj.validateRnc();
+        System.out.println(allErros);
+        
     }
 
-    public static void validateRnc() throws IOException {
+    public List<List<String>> validateRnc() throws IOException {
         File schemaLocation = new File("/Users/debaditya.mohankudo/Documents/SPIKE-validate-BIMI-LOGO/relaxng/relaxng/svg_1-2_ps.rnc");
         File svgLocation = new File("/Users/debaditya.mohankudo/Documents/SPIKE-validate-BIMI-LOGO/relaxng/relaxng/xss-mouseover.svg");
         System.setProperty(SchemaFactory.class.getName() + ":" + XMLConstants.RELAXNG_NS_URI, "com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory");
@@ -54,7 +61,7 @@ class RelaxNGCompactSchemaValidator {
             eh.printException(e);
         }
 
-        System.out.println(ceh.allErrors);
+        return eh.getAllErrors();
     }
 
 }
