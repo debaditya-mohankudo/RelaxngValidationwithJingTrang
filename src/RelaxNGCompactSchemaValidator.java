d3f7b8c214e359bea7cc1a7bfef235f3dcd5673b
debaditya.mohankudo <debaditya.mohankudo@digicert.com>
2021-05-19 09:12:24 +0530

    commit

--- src/RelaxNGCompactSchemaValidator.java
+++ src/RelaxNGCompactSchemaValidator.java
@@ -19,32 +19,49 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 
+import org.xml.sax.SAXParseException;
+
 
 class RelaxNGCompactSchemaValidator { 
     
-    private  class CustomErrorHandler extends ErrorHandlerImpl {
+    private class CustomErrorHandler extends ErrorHandlerImpl {
 
-        private List<List<String>> allErrors = new ArrayList<List<String>>();
-        public void print(String message) {
-            if (message.length() != 0) {
-                List<String> error = Arrays.asList(message.split(";"));
-                allErrors.add(error);
-            }
+        private final List<String> errors = new ArrayList<>();
+
+        @Override
+        public void error(SAXParseException e) {
+            addError(e);
         }
 
-        public List<List<String>> getAllErrors() {
-            return allErrors;
+        @Override
+        public void fatalError(SAXParseException e) {
+            addError(e);
+        }
+
+        private void addError(SAXParseException e) {
+            String error = e.getMessage();
+
+            int extraMessage = e.getMessage().indexOf("; expected ");
+            if (extraMessage > 0) {
+                error = e.getMessage().substring(0, extraMessage);
+            }
+
+            errors.add(error + " at line " + e.getLineNumber() + ":" + e.getColumnNumber());
+        }
+
+        public List<String> getErrors() {
+            return errors;
         }
     }
 
     public static void main(String[] args) throws IOException, SAXNotRecognizedException, SAXNotSupportedException {
         RelaxNGCompactSchemaValidator obj = new RelaxNGCompactSchemaValidator();
-        List<List<String>> allErros = obj.validateRnc();
+        List<String> allErros = obj.validateRnc();
         System.out.println(allErros);
         
     }
 
-    public List<List<String>> validateRnc() throws IOException, SAXNotRecognizedException, SAXNotSupportedException {
+    public List<String> validateRnc() throws IOException, SAXNotRecognizedException, SAXNotSupportedException {
         File schemaLocation = new File("./svg_1-2_ps.rnc");
         File svgLocation = new File("./bimi-sq-old.svg");
         System.setProperty(SchemaFactory.class.getName() + ":" + XMLConstants.RELAXNG_NS_URI, "com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory");
@@ -61,10 +78,10 @@ class RelaxNGCompactSchemaValidator {
         }
           
         catch (SAXException | IOException  e) {
-            eh.printException(e);
+            // eh.printException(e);
         }
 
-        return eh.getAllErrors();
+        return eh.getErrors();
     }
 
 }
